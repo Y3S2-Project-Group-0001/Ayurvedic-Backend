1d8a0dc0885e8e0c7cc4cf83eaa3413c18687ff7
Semora <senalweerasekara@gmail.com>
2023-04-18 22:29:02 +0530

    test

---
+++ delivery-service/src/models/deliveryDetails.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const deliveryDetailsSchema = new Schema({
+    CID:{
+        type: String,
+        required: false
+    },
+    Addresses: [{
+        HouseNo:{
+            type: String,
+            required: false
+        },
+        Address:{
+            type: String,
+            required: false
+        },
+        city:{
+            type: String,
+            required: false
+        },
+        street:{
+            type: String,
+            required: false
+        },
+    }],
+}, {timestamps : true})
+
+module.exports = mongoose.model('DeliveryDetails', deliveryDetailsSchema)
