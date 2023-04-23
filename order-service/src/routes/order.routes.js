4ca72f462a910bb764370ce672c60d57aae88829
Nisal-Sashmitha <it20081034@my.sliit.lk>
2023-04-23 17:18:40 +0530

    feat: added ymal and docker files to integrtor and order services

--- order-service/src/routes/order.routes.js
+++ order-service/src/routes/order.routes.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import { addNewCart, getCustomerCart } from '../controller/cart'
+import { addNewOrder, getCustomerOrders, approveOrder, declineOrder } from '../controller/order'
 
 const orderRouter = express.Router()
 
 orderRouter.post('/addCart', addNewCart)
 orderRouter.post('/getCustomerCart', getCustomerCart)
+orderRouter.post('/addNewOrder', addNewOrder)
+orderRouter.post('/getCustomerOrders', getCustomerOrders)
+orderRouter.post('/approveOrder', approveOrder)
+orderRouter.post('/declineOrder', declineOrder)
 // orderRouter.post('/editUser', editUser)
 // orderRouter.post('/getOneUser', getOneUser)
 // orderRouter.post('/delete', deleteUser)
