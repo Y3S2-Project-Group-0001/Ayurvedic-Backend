234779647553a8f3b33d5100416155a29782976e
Semora <senalweerasekara@gmail.com>
2023-04-26 12:24:22 +0530

    commit of semora

--- integrator-service/index.js
+++ integrator-service/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const proxy = require("express-http-proxy");
 
+
 const app = express();
 
 app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/order", proxy("http://localhost:8004/"));
